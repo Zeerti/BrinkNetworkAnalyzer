084ac21f5b5f66ede7fb49023b76aeb5ee518ba2
Zeerti <skarrj@gmail.com>
2017-01-24 17:27:37 -0700

    Added Port Scanning
    Added port scanning for all Active IP addresses.
    These are IP addresses, derived from ARP table, that respond to pings.
    Currently only scans one port that is hard coded. Will need to update to
    have dynamic port or even port ranges.

--- ScanNetwork.py
+++ ScanNetwork.py
@@ -15,6 +15,7 @@ class ScanNetwork():
 	def __init__(self, max_Threads):
 		self.ipList = list() #List of IP Address found on ARP Table
 		self.activeIPList = list()
+		self.knownRegisterList = list()
 		
 		self.totalIPFound = 0
 		self.pingPass = 0
@@ -65,6 +66,21 @@ class ScanNetwork():
 			worker = Thread(target=self.do_stuff, args=(self.primaryQueue,)) #target is the function that pops off queue args = arguments for queue function
 			worker.setDaemon(True)
 			worker.start()
+
+	def _Scan_IP_Port(self, address, port):
+		# Create a TCP socket
+		socketConnection = socket.socket()
+		#print ("Attempting to connect to {} on port {}".format(self.activeIPList[address], port))
+		try:
+			socketConnection.connect((self.activeIPList[address], port)) #Double Parenthesis needed as it takes a single touple for paramaters 
+			#print ("IP {} successfully opened port {}".format(self.activeIPList[address], port))
+			self.knownRegisterList.append(self.activeIPList[address])
+			return True
+		except socket.error:
+			#print ("IP {} failed to open port {}".format(self.activeIPList[address], port))
+			return False
+
+
 
 	def _get_ipList_Size(self):
 		return len(self.ipList)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import ScanNetwork
 
+port = 80
+
 if __name__ == "__main__":
 
-	scan = ScanNetwork.ScanNetwork(5) #Create class, pass number of threads
+	scan = ScanNetwork.ScanNetwork(10) #Create class, pass number of threads
 	scan._get_IP_From_ARP_Table()
 
 	#Add ping scan function to queue
@@ -14,14 +16,19 @@ if __name__ == "__main__":
 	scan.primaryQueue.join() #Wait until _startScan() has finished 
 
 	#Add port scanning to queue
-	#for currentIP in range(scan._get_ipList_Size()):
-	#	scan.primaryQueue.put(lambda currentIP = currentIP: scan._Scan_IP_Ports(currentIP))
+	#Check Port 80 for testing, eventually check 10051
+
+	for currentIP in range(scan._get_active_ipList_Size()):
+		scan.primaryQueue.put(lambda currentIP = currentIP, port = port: scan._Scan_IP_Port(currentIP, port))
+
+	scan._startScan()
+	scan.primaryQueue.join()
+
+	print(scan.knownRegisterList)
 
 	#Test port scanning
-	scan._scan_port(scan.activeIPList[0], 80)
+	#scan._scan_port(scan.activeIPList[0], 80)
 	
-
-	print ("{} active IP Addresses".format(scan._get_active_ipList_Size()))
 
 
 
