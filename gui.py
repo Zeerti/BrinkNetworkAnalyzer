a8b72d06bafdab4549972afb473b8f268f99dd96
Zeerti <skarrj@gmail.com>
2017-01-28 20:46:56 -0700

    Port Scanning / IP Scanning Completed
    Added in port scanning and IP scanning into the GUI. Should show
    correctly as well. Needs to still be tweaked

--- gui.py
+++ gui.py
@@ -1,4 +1,28 @@
+"""
+#Determine how to Identify The following: -- HIGHEST PRIORITY
+#Register 
+    Scan Lab Registers for open ports, find similarities
+#Network Printer
+#Tablet
+    Scan Lab Tablet for open ports. Research Standard android ports open
+#Verifone
+    Scan Lab Verifone for open ports.
+
+#unmanaged Switch(s) -- May not be possible.
+
+One way is to list the mac address table on the managed switches
+and look for ports with multiple mac addresses with are not links
+to other known switches. Then, using arp lookup and ping -a, you
+can find the ip address/dns names of the hosts connected to the
+unmanaged switches.
+
+
+#Create Custom Header Renderer to change appearance. -- LOW
+
+"""
+
 import wx
+import wx.lib.agw.ultimatelistctrl as ULC
 import scan
 
 ########################################################################
@@ -7,23 +31,32 @@ class GUI(wx.Frame):
     #----------------------------------------------------------------------
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY, "Network Analyzer")
-
+        self.window = wx.GetActiveWindow()
         self.scan = scan.ScanNetwork(10)                                                        #Init scanNetwork class, 10 threads
 
         self.SetSize(1200,900)                                                                  #Set window size
         self.Centre()     
         self.version = '0.0.2'    
 
-        self.ipFont = wx.Font(18, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)      
-        self.detailedFont = wx.Font(14, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
-        self.index = 0
+        self.ipFont = wx.Font(14, wx.MODERN, wx.NORMAL, wx.NORMAL)      
+        self.detailedFont = wx.Font(12, wx.SWISS, wx.NORMAL, wx.NORMAL)
+        self.InsertCount = 0
+        self.currentSelection = None
         
 
-
+        #Theme Pastel
         self.lightTan = (221,216,184)
         self.deepPurple = (84,46,113)
         self.lightBlue = (132,169,192)
-        self.lightPurple = (106,102,163)                                                                  #Center window on display
+        self.lightPurple = (106,102,163) 
+
+        #Theme Brink
+        self.lightOrange = (226,130,113) 
+        self.darkGrey = (61,62,67)
+        self.darkBlack = (41,42,47)
+        self.brinkBlue = (36,118,185)
+        self.lightGrey = (238,238,238)                                                                  
+        self.white = (255,255,255)
 
         #Create Menu Bar
         menubar = wx.MenuBar()                                                                  #Parent Menu Viewport
@@ -38,6 +71,7 @@ class GUI(wx.Frame):
 
         scanIPOnlyMenuItem = scanMenu.Append(wx.ID_ANY, 'Scan IP\tCtrl+I',
                                                         'Scan Only for IP Addresses')
+        scanPortsMenuItem = scanMenu.Append(wx.ID_ANY, 'Scan Open Ports')
         menubar.Append(scanMenu, '&Scan')
 
 
@@ -49,75 +83,98 @@ class GUI(wx.Frame):
         versionMenu.Append(wx.ID_ANY, self.version)
         helpMenu.Append(wx.ID_ANY, '&Version', versionMenu)
 
-
-
         self.SetMenuBar(menubar)                                                                #Add parent menu object to parent frame
 
         self.Bind(wx.EVT_MENU, self.OnQuit, quitMenuItem)                                       #Bind quit function to menu click event
         self.Bind(wx.EVT_MENU, self.ScanNetwork, scanMenuItem)                                  #Bind scan function to menu click event
         self.Bind(wx.EVT_MENU, self.DisplayHelp, helpMenuItem)                                  #Bind help display function to menu click event
+        self.Bind(wx.EVT_MENU, self.ScanOpenPortsOnTargetIP, scanPortsMenuItem)
         #self.Bind(wx.EVT_MENU, self.DisplayVersion, versionMenu)                            #Bind version display function to menu click event
  
         # Create Panels for viewports
-        panel = wx.Panel(self, wx.ID_ANY, pos=(0,0), size=(200,863))                            #Panel to contain listctrl ipListControl
-        panel2 = wx.Panel(self, wx.ID_ANY, pos=(201,0), size=(990,863))                         #Panel to contain listctrl detailedListControl
+        panel = wx.Panel(self, wx.ID_ANY, pos=(0,0), size=(200,863))                            #Panel to contain listctrl ipULC
+        panel2 = wx.Panel(self, wx.ID_ANY, pos=(200,0), size=(990,863))                         #Panel to contain listctrl detailedListControl
         
  
         #Add List Control Class to IP Viewport#
-        self.ipListControl = wx.ListCtrl(panel, size=(200,863),
-                         style=wx.LC_REPORT
-                         |wx.BORDER_NONE)
+        self.ipULC = ULC.UltimateListCtrl(panel, wx.ID_ANY, size=(200,863),
+                                        agwStyle=wx.LC_REPORT|
+                                                wx.LC_VRULES|
+                                                wx.LC_SINGLE_SEL|
+                                                wx.BORDER_NONE)
 
         #Add List Control Class to Detailed Viewport#
-        self.detailedListControl = wx.ListCtrl(panel2, size=(990,863),
-                         style=wx.LC_REPORT
-                         |wx.BORDER_NONE)
+        self.detailedULC = ULC.UltimateListCtrl(panel2, wx.ID_ANY, size=(990,863),
+                                                agwStyle=wx.LC_REPORT|
+                                                        wx.LC_VRULES|
+                                                        wx.LC_SINGLE_SEL|
+                                                        ULC.ULC_NO_HIGHLIGHT|
+                                                        wx.BORDER_NONE)
 
         #Configure IP Viewport#
-        self.ipListControl.InsertColumn(0, 'Discovered')
-        self.ipListControl.SetColumnWidth(0, 200)
-        self.ipListControl.SetBackgroundColour(self.lightBlue)
-        self.ipListControl.SetTextColour(self.deepPurple)
-        #for i in range(0,15):
-        #    self.AddToListCtrl(self.ipListControl, 0, "192.168.1." + str(i))
-        self.ipListControl.SetFont(self.ipFont)
-        self.ipListControl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnIPListClick)
+        self.ipULC.InsertColumn(0, 'Discovered')
+        self.ipULC.SetColumnWidth(0, 200)
+        self.ipULC.SetBackgroundColour(self.darkBlack)
+        self.ipULC.SetTextColour(self.brinkBlue)
+        self.ipULC.SetScrollbar(0,0,0,0, refresh=False)
+        self.ipULC.SetFont(self.ipFont)
+        self.ipULC.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnIPListClick)
+
 
 
         #Configure Detailed Viewport# 
-        self.detailedListControl.InsertColumn(1, 'Detailed Information ')
-        self.detailedListControl.SetColumnWidth(0, 990)
-        self.detailedListControl.SetBackgroundColour(self.lightPurple)
-        self.detailedListControl.SetTextColour(self.lightTan)
-        self.detailedListControl.SetFont(self.detailedFont)
+        self.detailedULC.InsertColumn(0, 'Detailed Information ')
+        self.detailedULC.SetColumnWidth(0, 990)
+        self.detailedULC.SetBackgroundColour(self.darkGrey)
+        self.detailedULC.SetTextColour(self.lightOrange)
+        self.detailedULC.SetFont(self.detailedFont)
+        
         #for i in range(0,15):
-        #    self.AddToListCtrl(self.detailedListControl, 0, "192.168.1." + str(i))
+        #    self.AddToListCtrl(self.detailedULC, 0, "192.168.1." + str(i))
+
+        #Ultimate List Control Test
+
+       
+        #self.list.SetPalette(7)
 
 
  
     ##BEGIN FUNCTIONS##
+    
 
     #----------------------------------------------------------------------
     def add_line(self, event):
         line = "Line %s" % self.index
-        self.ipListControl.InsertStringItem(self.index, line)
-        self.ipListControl.SetStringItem(self.index, 1, "01/19/2010")
-        self.ipListControl.SetStringItem(self.index, 2, "USA")
+        self.ipULC.InsertStringItem(self.index, line)
+        self.ipULC.SetStringItem(self.index, 1, "01/19/2010")
+        self.ipULC.SetStringItem(self.index, 2, "USA")
         self.index += 1
 
     #----------------------------------------------------------------------
     def OnIPListClick(self, event):
-        print(event.GetText())
-        #Insert code to display 
+        self.currentSelection = event.GetIndex()
+        for i in range(0, self.InsertCount):
+            if(self.InsertCount > 0):
+                #print("Deleting Item Index {}".format(i))
+                self.detailedULC.DeleteItem(i)
+                self.InsertCount -= 1
+
+        self.AddToListCtrl(self.detailedULC, self.InsertCount, self.scan._Get_Ping_Statistics(event.GetIndex()))
+        self.InsertCount += 1
+        #self.detailedULC.InsertStringItem(self.detailedIndex, self.scan._Get_Ping_Statistics(event.GetIndex()))
 
     #----------------------------------------------------------------------
     #
-    def AddToListCtrl(self, listctrl, column, item):
+    def AddToListCtrl(self, listctrl, position, item):
         TempListCtrl = listctrl
-        TempColumn = column
         TempItem = item
+        TempPosition = position
 
-        TempListCtrl.InsertItem(TempColumn, TempItem)
+        TempListCtrl.InsertStringItem(TempPosition, TempItem)
+
+        TempListCtrl = None
+        TempItem = None
+        TempPosition = None
 
      #----------------------------------------------------------------------
     def OnQuit(self, event):
@@ -133,18 +190,41 @@ class GUI(wx.Frame):
         self.scan.primaryQueue.join() #Wait for queue to finish
 
         for activeIP in range(self.scan._get_active_ipList_Size()):
-            self.AddToListCtrl(self.ipListControl, 0, self.scan.activeIPList[activeIP])
+            self.AddToListCtrl(self.ipULC, activeIP, self.scan.activeIPList[activeIP])
+            
 
+    #----------------------------------------------------------------------
+    def ScanOpenPortsOnTargetIP(self, event):
+
+        for currentPort in range(0, 10000):
+            #print("{} CURRENT PORT".format(currentPort))
+            self.scan.primaryQueue.put(lambda ipaddress = self.currentSelection, currentPort = currentPort: self.scan._Scan_IP_Port(ipaddress, currentPort))
+
+        self.scan._startScan()
+        self.scan.primaryQueue.join() #wait until all IPs have been scanned on port, port.
+
+        #Add all entries into detailed view list
+        for ports in range(0, self.scan._get_Open_Ports_Size()):
+            self.AddToListCtrl(self.detailedULC, self.InsertCount, self.scan._get_Open_Ports(ports))
+            self.InsertCount += 1
+
+        self.AddToListCtrl(self.detailedULC, self.InsertCount, "Current Open Ports")
+        self.InsertCount += 1
+        #print(self.scan._get_Open_Ports_Size)
+        #print(self.scan._get_Open_Ports(0))
+        
 
         
 
     #----------------------------------------------------------------------
+    #Function may not be needed
     def DisplayHelp(self, event):
         self.Close()
 
     #----------------------------------------------------------------------
+    #Function may not be needed
     def DisplayVersion(self, event):
-        self.Close()
+        self.Close() 
  
 #----------------------------------------------------------------------
 # Run the program
--- scan.py
+++ scan.py
@@ -10,12 +10,15 @@ from threading import Thread
 import re
 import os
 import subprocess
+from subprocess import Popen, PIPE
 
 class ScanNetwork():
 	def __init__(self, max_Threads):
 		self.ipList = list() #List of IP Address found on ARP Table
+		self.pingStatistics = list()
 		self.activeIPList = list()
-		self.knownRegisterList = list()
+		self.openPortList = list() #Stores all open Ports for a --SINGLE-- ipadress MUST BE CLEARED BEFORE REUSING
+		self.openPortIPList = list()
 		
 		self.totalIPFound = 0
 		self.pingPass = 0
@@ -25,22 +28,23 @@ class ScanNetwork():
 		self.primaryQueue = queue.Queue(maxsize=0)
 		self.num_threads = max_Threads
 
+	#----------------------------------------------------------------------
 	#get queue task
 	def do_stuff(self, primaryQueue):
 		while True:
 			currentOperation = self.primaryQueue.get()
 			currentOperation()
 			self.totalJobsCompleted += 1
-			#print("Completed {} queue Jobs".format(self.totalJobsCompleted))
 			self.primaryQueue.task_done()
 
 
+	#----------------------------------------------------------------------
 	#get list of active ip addresses (IP's that have been pinged)
 	def _get_Active_IP_Addresses(self, currentIPIteration):
-		cmdCommand = subprocess.Popen(['ping', '-n', '1', self.ipList[currentIPIteration]])
-		streamdata = cmdCommand.communicate()[0]
+		cmdCommand = Popen(['ping', '-n', '1', self.ipList[currentIPIteration]], stdout=PIPE)
+		text = cmdCommand.communicate()[0]
+		self.pingStatistics.append(text)
 		cmdReturnCode = cmdCommand.returncode
-		#print("\n\n\nJob# {}, Return Code {}".format(currentIPIteration, cmdReturnCode))
 
 		if cmdReturnCode == 0:
 			self.activeIPList.append(self.ipList[currentIPIteration])
@@ -49,6 +53,7 @@ class ScanNetwork():
 		elif cmdReturnCode == 3: #General Failure to ping
 			pass
 
+	#----------------------------------------------------------------------
 	def _get_IP_From_ARP_Table(self): #Regular Expression Pulls IP Addresses out from ARP Table
 		arpLines = os.popen('arp -a') #Runs ARP CMD, saves results into memory
 		regExpFindIPAddress =  re.compile ('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
@@ -60,6 +65,7 @@ class ScanNetwork():
 				self.ipList.append(ipMatch.group())
 				self.totalIPFound += 1
 
+	#----------------------------------------------------------------------
 	#Create threads and start them working the queue
 	def _startScan(self):
 		for i in range(self.num_threads):
@@ -67,26 +73,47 @@ class ScanNetwork():
 			worker.setDaemon(True)
 			worker.start()
 
+	
+
+	#----------------------------------------------------------------------
 	def _Scan_IP_Port(self, address, port):
 		# Create a TCP socket
-		socketConnection = socket.socket()
+		socketConnection = socket.socket() 
+		socketConnection.settimeout(10)
+		TempAddress = str(address)
+		TempPort = port
 		#print ("Attempting to connect to {} on port {}".format(self.activeIPList[address], port))
 		try:
-			socketConnection.connect((self.activeIPList[address], port)) #Double Parenthesis needed as it takes a single touple for paramaters 
+			socketConnection.connect((TempAddress, TempPort)) #Double Parenthesis needed as it takes a single touple for paramaters 
 			#print ("IP {} successfully opened port {}".format(self.activeIPList[address], port))
-			self.knownRegisterList.append(self.activeIPList[address])
+			self.openPortList.append("{} ||".format(port))
+			print("SUUUUUUUUUUUUUUUUUUUUUCCCCCCCCCCCCCCEEEEEEEEEESSSSSSSSSSSSS\n\n\n\n\n\n\n\n\n\n\n\n")
 			return True
-		except socket.error:
-			#print ("IP {} failed to open port {}".format(self.activeIPList[address], port))
+		except socket.error as err:
+			print ("IP {} failed to open port {}, ERROR: {}".format(self.activeIPList[address], port, err))
+			socket.error()
 			return False
 
 
-
+	#----------------------------------------------------------------------
 	def _get_ipList_Size(self):
 		return len(self.ipList)
 
+	#----------------------------------------------------------------------
+	def _get_Open_Ports(self, iteration):
+		return self.openPortList[iteration]
+
+	#----------------------------------------------------------------------
+	def _get_Open_Ports_Size(self):
+		return len(self.openPortList)
+
+	#----------------------------------------------------------------------
 	def _get_active_ipList_Size(self):
 		return len(self.activeIPList)
+
+	#----------------------------------------------------------------------
+	def _Get_Ping_Statistics(self, iteration):
+		return self.pingStatistics[iteration]
 
 	
 
